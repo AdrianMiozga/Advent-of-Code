d82e3f2d64c456229b93dc2c80403d8cf75f156a
Adrian Miozga <AdrianMiozga@outlook.com>
2023-05-19 16:05:52 +0200

    Upate day 3 of 2015

--- 2015/03/03.py
+++ 2015/03/03.py
@@ -1,46 +1,82 @@
-santa_position = [0, 0]
-robo_santa_position = [0, 0]
+FILENAME = "2015/03/input.txt"
 
-santa = True
 
-places = [[0, 0]]
+def part_one():
+    santa_position = [0, 0]
 
-with open("input.txt", "r") as file:
-    for line in file:
-        for ch in line:
-            if ch == ">":
-                if santa:
+    places = [[0, 0]]
+
+    with open(FILENAME, "r", encoding="UTF-8") as file:
+        for line in file:
+            for char in line:
+                if char == ">":
                     santa_position[0] += 1
-                else:
-                    robo_santa_position[0] += 1
-            elif ch == "<":
-                if santa:
+                elif char == "<":
                     santa_position[0] -= 1
-                else:
-                    robo_santa_position[0] -= 1
-            elif ch == "^":
-                if santa:
+                elif char == "^":
                     santa_position[1] += 1
-                else:
-                    robo_santa_position[1] += 1
-            elif ch == "v":
-                if santa:
+                elif char == "v":
                     santa_position[1] -= 1
-                else:
-                    robo_santa_position[1] -= 1
 
-            if santa:
                 places.append(list(santa_position))
-            else:
-                places.append(list(robo_santa_position))
 
-            santa = not santa
+    unique_places = []
 
-unique_places = []
+    for place in places:
+        if place not in unique_places:
+            unique_places.append(place)
 
-for x in places:
-    if x not in unique_places:
-        unique_places.append(x)
+    print(len(unique_places))
 
-print(len(places))
-print(len(unique_places))
+
+def part_two():
+    santa_position = [0, 0]
+    robo_santa_position = [0, 0]
+
+    santa = True
+
+    places = [[0, 0]]
+
+    with open(FILENAME, "r", encoding="UTF-8") as file:
+        for line in file:
+            for char in line:
+                if char == ">":
+                    if santa:
+                        santa_position[0] += 1
+                    else:
+                        robo_santa_position[0] += 1
+                elif char == "<":
+                    if santa:
+                        santa_position[0] -= 1
+                    else:
+                        robo_santa_position[0] -= 1
+                elif char == "^":
+                    if santa:
+                        santa_position[1] += 1
+                    else:
+                        robo_santa_position[1] += 1
+                elif char == "v":
+                    if santa:
+                        santa_position[1] -= 1
+                    else:
+                        robo_santa_position[1] -= 1
+
+                if santa:
+                    places.append(list(santa_position))
+                else:
+                    places.append(list(robo_santa_position))
+
+                santa = not santa
+
+    unique_places = []
+
+    for place in places:
+        if place not in unique_places:
+            unique_places.append(place)
+
+    print(len(unique_places))
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
