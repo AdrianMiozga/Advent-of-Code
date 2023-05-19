7226530dcab6bfb75ab677a80d5631888d7a779f
Adrian Miozga <AdrianMiozga@outlook.com>
2023-05-19 16:13:33 +0200

    Update day 4 of 2015

--- 2015/04/04.py
+++ 2015/04/04.py
@@ -1,14 +1,42 @@
 import hashlib
 
-input = "yzbqklnj"
-i = 0
+FILENAME = "2015/04/input.txt"
 
-while True:
-    i += 1
 
-    test = input + str(i)
-    result = hashlib.md5(test.encode("utf-8")).hexdigest()
+def part_one():
+    with open(FILENAME, "r", encoding="UTF-8") as file:
+        key = file.readline()
 
-    if result.startswith("000000"):
-        print(i)
-        break
+    i = 0
+
+    while True:
+        i += 1
+
+        test = key + str(i)
+        result = hashlib.md5(test.encode("UTF-8")).hexdigest()
+
+        if result.startswith("00000"):
+            print(i)
+            break
+
+
+def part_two():
+    with open(FILENAME, "r", encoding="UTF-8") as file:
+        key = file.readline()
+
+    i = 0
+
+    while True:
+        i += 1
+
+        test = key + str(i)
+        result = hashlib.md5(test.encode("UTF-8")).hexdigest()
+
+        if result.startswith("000000"):
+            print(i)
+            break
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
