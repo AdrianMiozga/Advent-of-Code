9ceff7c9b59d5db6447fe814f837b66c10c42256
Adrian Miozga <AdrianMiozga@outlook.com>
2023-05-19 15:53:53 +0200

    Update day 1 of 2015

--- 2015/01/01.py
+++ 2015/01/01.py
@@ -1,14 +1,36 @@
-opening_bracket = 0
-closing_bracket = 0
+FILENAME = "2015/01/input.txt"
 
-with open("input.txt", "r") as file:
-    for line in file:
-        for ch in line:
-            if ch == "(":
-                opening_bracket += 1
-            elif ch == ")":
-                closing_bracket += 1
 
-floor = opening_bracket - closing_bracket
+def part_one():
+    floor = 0
 
-print(floor)
+    with open(FILENAME, "r", encoding="UTF-8") as file:
+        for line in file:
+            for char in line:
+                if char == "(":
+                    floor += 1
+                elif char == ")":
+                    floor -= 1
+
+    print(floor)
+
+
+def part_two():
+    floor = 0
+
+    with open(FILENAME, "r", encoding="UTF-8") as file:
+        for line in file:
+            for index, char in enumerate(line):
+                if char == "(":
+                    floor += 1
+                elif char == ")":
+                    floor -= 1
+
+                if floor == -1:
+                    print(index + 1)
+                    return
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
