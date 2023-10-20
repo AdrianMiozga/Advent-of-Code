3f2f2e3133abbc87986f3d58cf97915a3195d83f
Adrian Miozga <AdrianMiozga@outlook.com>
2023-10-20 21:00:20 +0200

    Add day 8 of 2020

---
+++ 2020/08/08.kt
@@ -0,0 +1,83 @@
+import java.io.File
+import java.util.List.copyOf
+
+private const val FILENAME = "2020/08/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    var accumulator = 0
+
+    var quit = false
+    var index = 0
+
+    val lines = File(FILENAME).readLines()
+    val used = BooleanArray(lines.size)
+
+    do {
+        val instruction = lines[index]
+        used[index] = true
+
+        if (instruction.startsWith("nop")) {
+            index++
+        } else if (instruction.startsWith("acc")) {
+            accumulator += instruction.split(" ")[1].toInt()
+            index++
+        } else {
+            index += instruction.split(" ")[1].toInt()
+        }
+
+        if (used[index]) {
+            quit = true
+        }
+    } while (!quit)
+
+    println(accumulator)
+}
+
+private fun partTwo() {
+    val lines = File(FILENAME).readLines()
+
+    for ((lineIndex, line) in lines.withIndex()) {
+        val alteredInput = copyOf(lines).toMutableList()
+
+        if (line.startsWith("nop")) {
+            alteredInput[lineIndex] =
+                alteredInput[lineIndex].replace("nop", "jmp")
+        } else if (line.startsWith("jmp")) {
+            alteredInput[lineIndex] =
+                alteredInput[lineIndex].replace("jmp", "nop")
+        } else {
+            continue
+        }
+
+        var accumulator = 0
+        var index = 0
+        var quit = false
+        val used = BooleanArray(lines.size)
+
+        do {
+            val instruction = alteredInput[index]
+            used[index] = true
+
+            if (instruction.startsWith("nop")) {
+                index++
+            } else if (instruction.startsWith("acc")) {
+                accumulator += instruction.split(" ")[1].toInt()
+                index++
+            } else {
+                index += instruction.split(" ")[1].toInt()
+            }
+
+            if (index >= lines.size) {
+                println(accumulator)
+                return
+            } else if (used[index]) {
+                quit = true
+            }
+        } while (!quit)
+    }
+}
---
+++ 2020/08/README.md
@@ -0,0 +1,6 @@
+# [Day 8 of 2020](https://adventofcode.com/2020/day/8)
+
+## Expected Results
+
+- Part 1: 1915
+- Part 2: 944
