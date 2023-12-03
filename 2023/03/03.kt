a93aedb08c2dbd2c94e523b2cb19e67df7f7569b
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-03 15:38:20 +0100

    Add day 3 of 2023

---
+++ 2023/03/03.kt
@@ -0,0 +1,115 @@
+import java.io.File
+
+private const val FILENAME = "2023/03/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines().map { it.toCharArray() }
+    var result = 0
+
+    for (row in 1 until file.size - 1) {
+        for (column in 1 until file[row].size - 1) {
+            if (file[row][column].isDigit() || file[row][column] == '.') {
+                continue
+            }
+
+            for (windowRow in -1..1) {
+                for (windowColumn in -1..1) {
+                    val dRow = row - windowRow
+                    val dColumn = column - windowColumn
+
+                    if (file[dRow][dColumn].isDigit()) {
+                        var number = ""
+                        number = file[dRow][dColumn] + number
+                        file[dRow][dColumn] = '.'
+
+                        for (left in dColumn - 1 downTo 0) {
+                            if (file[dRow][left].isDigit()) {
+                                number = "${file[dRow][left]}$number"
+                                file[dRow][left] = '.'
+                            } else {
+                                break
+                            }
+                        }
+
+                        for (right in dColumn + 1 until file[dRow].size) {
+                            if (file[dRow][right].isDigit()) {
+                                number = "$number${file[dRow][right]}"
+                                file[dRow][right] = '.'
+                            } else {
+                                break
+                            }
+                        }
+
+                        result += number.toInt()
+                    }
+                }
+            }
+        }
+    }
+
+    println(result)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines().map { it.toCharArray() }
+    var result = 0
+
+    for (row in 1 until file.size - 1) {
+        for (column in 1 until file[row].size - 1) {
+            if (file[row][column] != '*') {
+                continue
+            }
+
+            var first = 0
+            var second = 0
+
+            for (windowRow in -1..1) {
+                for (windowColumn in -1..1) {
+                    val dRow = row - windowRow
+                    val dColumn = column - windowColumn
+
+                    if (file[dRow][dColumn].isDigit()) {
+                        var number = ""
+                        number = file[dRow][dColumn] + number
+                        file[dRow][dColumn] = '.'
+
+                        for (left in dColumn - 1 downTo 0) {
+                            if (file[dRow][left].isDigit()) {
+                                number = "${file[dRow][left]}$number"
+                                file[dRow][left] = '.'
+                            } else {
+                                break
+                            }
+                        }
+
+                        for (right in dColumn + 1 until file[dRow].size) {
+                            if (file[dRow][right].isDigit()) {
+                                number = "$number${file[dRow][right]}"
+                                file[dRow][right] = '.'
+                            } else {
+                                break
+                            }
+                        }
+
+                        if (first == 0) {
+                            first = number.toInt()
+                        } else {
+                            second = number.toInt()
+                        }
+                    }
+                }
+            }
+
+            if (first != 0 && second != 0) {
+                result += (first * second)
+            }
+        }
+    }
+
+    println(result)
+}
---
+++ 2023/03/README.md
@@ -0,0 +1,6 @@
+# [Day 3 of 2023](https://adventofcode.com/2023/day/3)
+
+## Expected Results
+
+- Part 1: 540131
+- Part 2: 86879020
