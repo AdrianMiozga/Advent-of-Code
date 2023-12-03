49d7d57cc4e6a9988a784260e0ce45c712294067
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-03 16:49:23 +0100

    Add day 2 of 2023

---
+++ 2023/02/02.kt
@@ -0,0 +1,89 @@
+import java.io.File
+
+private const val FILENAME = "2023/02/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+    var result = 0
+
+    for (line in file) {
+        val regex = Regex("""Game (\d+): """)
+        val gameID = regex.find(line)!!.groupValues[1].toInt()
+
+        val sets = line.split(";")
+        var valid = true
+
+        for (set in sets) {
+            val red =
+                Regex("""(\d+) red""").find(set)?.groupValues?.get(1)?.toInt()
+                    ?: 0
+
+            val green =
+                Regex("""(\d+) green""").find(set)?.groupValues?.get(1)?.toInt()
+                    ?: 0
+
+            val blue =
+                Regex("""(\d+) blue""").find(set)?.groupValues?.get(1)?.toInt()
+                    ?: 0
+
+            if (red > 12 || green > 13 || blue > 14) {
+                valid = false
+                break
+            }
+        }
+
+        if (valid) {
+            result += gameID
+        }
+    }
+
+    println(result)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines()
+    var result = 0
+
+    for (line in file) {
+        var minRed = 1
+        var minGreen = 1
+        var minBlue = 1
+
+        val sets = line.split(";")
+
+        for (set in sets) {
+            val red =
+                Regex("""(\d+) red""").find(set)?.groupValues?.get(1)?.toInt()
+                    ?: 1
+
+            val green =
+                Regex("""(\d+) green""").find(set)?.groupValues?.get(1)?.toInt()
+                    ?: 1
+
+            val blue =
+                Regex("""(\d+) blue""").find(set)?.groupValues?.get(1)?.toInt()
+                    ?: 1
+
+            if (minRed < red) {
+                minRed = red
+            }
+
+            if (minGreen < green) {
+                minGreen = green
+            }
+
+            if (minBlue < blue) {
+                minBlue = blue
+            }
+        }
+
+        result += (minRed * minGreen * minBlue)
+    }
+
+    println(result)
+}
---
+++ 2023/02/README.md
@@ -0,0 +1,6 @@
+# [Day 2 of 2023](https://adventofcode.com/2023/day/2)
+
+## Expected Results
+
+- Part 1: 1931
+- Part 2: 83105
