0bbda4b071cb8cedfe64b764075477920b29076b
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-09 00:46:18 +0100

    Add day 2 of 2017

---
+++ 2017/02/02.kt
@@ -0,0 +1,61 @@
+import java.io.File
+
+private const val FILENAME = "2017/02/input.txt"
+private val regex = Regex("""\s+""")
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+        .map { it -> it.split(regex).map { it.toInt() } }
+
+    var result = 0
+
+    for (line in file) {
+        var smallest = Integer.MAX_VALUE
+        var biggest = 0
+
+        for (element in line) {
+            if (element < smallest) {
+                smallest = element
+            }
+
+            if (element > biggest) {
+                biggest = element
+            }
+        }
+
+        result += (biggest - smallest)
+    }
+
+    println(result)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines()
+        .map { it -> it.split(regex).map { it.toInt() } }
+
+    var result = 0
+
+    for (line in file) {
+        val sorted = line.sortedDescending()
+
+        outer@ for (i in sorted) {
+            for (j in sorted.reversed()) {
+                if (i == j) {
+                    continue
+                }
+
+                if (i % j == 0) {
+                    result += (i / j)
+                    break@outer
+                }
+            }
+        }
+    }
+
+    println(result)
+}
---
+++ 2017/02/README.md
@@ -0,0 +1,6 @@
+# [Day 2 of 2017](https://adventofcode.com/2017/day/2)
+
+## Expected Results
+
+- Part 1: 41919
+- Part 2: 303
