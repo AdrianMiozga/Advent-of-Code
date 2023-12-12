924251ffd802b9c13b44927e2b7fd08b0e034f20
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-12 22:33:18 +0100

    Add day 4 of 2022

---
+++ 2022/04/04.kt
@@ -0,0 +1,50 @@
+import java.io.File
+
+private const val FILENAME = "2022/04/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+
+    var count = 0
+
+    for (line in file) {
+        val (firstX, firstY, secondX, secondY) = Regex("""(\d+)-(\d+),(\d+)-(\d+)""").matchEntire(line)!!.destructured.toList()
+            .map { it.toInt() }
+
+        if (secondX >= firstX && secondY <= firstY) {
+            count++
+        } else if (firstX >= secondX && firstY <= secondY) {
+            count++
+        }
+    }
+
+    println(count)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines()
+
+    var count = 0
+
+    for (line in file) {
+        val (firstX, firstY, secondX, secondY) = Regex("""(\d+)-(\d+),(\d+)-(\d+)""").matchEntire(line)!!.destructured.toList()
+            .map { it.toInt() }
+
+        if (firstX in secondX..secondY) {
+            count++
+        } else if (firstY in secondX..secondY) {
+            count++
+        } else if (secondX in firstX..firstY) {
+            count++
+        } else if (secondY in firstX..firstY) {
+            count++
+        }
+    }
+
+    println(count)
+}
---
+++ 2022/04/README.md
@@ -0,0 +1,6 @@
+# [Day 4 of 2022](https://adventofcode.com/2022/day/4)
+
+## Expected Results
+
+- Part 1: 534
+- Part 2: 841
