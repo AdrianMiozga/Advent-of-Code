1036d6142a2685ba6d0aec5d7f973d7f963351be
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-19 12:53:35 +0100

    Add part 1 of day 19 of 2023

---
+++ 2023/19/19.kt
@@ -0,0 +1,63 @@
+import java.io.File
+import java.util.*
+
+private const val FILENAME = "2023/19/input.txt"
+
+fun main() {
+    partOne()
+}
+
+private fun partOne() {
+    val (workflows, ratings) = File(FILENAME).readText().split("\r\n\r\n")
+        .map { it.split("\r\n") }
+
+    var result = 0
+
+    for (rating in ratings) {
+        val map =
+            rating.removeSurrounding("{", "}").split(",").associate {
+                val (key, value) = it.split("=")
+                key to value.toInt()
+            }
+
+        val sum = map.values.sum()
+
+        val stack = Stack<String>()
+        stack.push(workflows.find { it.substringBefore("{") == "in" })
+
+        while (stack.isNotEmpty()) {
+            val instructions =
+                stack.pop().substringAfter("{").substringBefore("}").split(",")
+
+            for (instruction in instructions) {
+                if (instruction.all { it.isLetter() }) {
+                    if (instruction == "A") {
+                        result += sum
+                    } else if (instruction != "R") {
+                        stack.push(workflows.find {
+                            it.substringBefore("{") == instruction
+                        })
+                    }
+
+                    break
+                }
+
+                val (letter, sign, value, ret) = Regex("""(\w)([<>])(\d+):(\w+)""").matchEntire(
+                    instruction
+                )!!.destructured
+
+                if ((sign == "<" && map[letter]!! < value.toInt()) || (sign == ">" && map[letter]!! > value.toInt())) {
+                    if (ret == "A") {
+                        result += sum
+                    } else if (ret != "R") {
+                        stack.push(workflows.find { it.substringBefore("{") == ret })
+                    }
+
+                    break
+                }
+            }
+        }
+    }
+
+    println(result)
+}
---
+++ 2023/19/README.md
@@ -0,0 +1,6 @@
+# [Day 19 of 2023](https://adventofcode.com/2023/day/19)
+
+## Expected Results
+
+- Part 1: 406849
+- Part 2: TODO
