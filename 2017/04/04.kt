0860442f6a67e52298ba09f076d7f4bd792676d6
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-21 01:41:53 +0100

    Add day 4 of 2017

---
+++ 2017/04/04.kt
@@ -0,0 +1,54 @@
+import java.io.File
+
+private const val FILENAME = "2017/04/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val passwords = File(FILENAME).readLines()
+
+    var count = 0
+
+    password@for (password in passwords) {
+        val words = HashSet<String>()
+
+        for (word in password.split(" ")) {
+            if (word in words) {
+                continue@password
+            } else {
+                words.add(word)
+            }
+        }
+
+        count++
+    }
+
+    println(count)
+}
+
+private fun partTwo() {
+    val passwords = File(FILENAME).readLines()
+
+    var count = 0
+
+    password@for (password in passwords) {
+        val words = HashSet<String>()
+
+        for (word in password.split(" ")) {
+            val sortedPassword = word.toCharArray().sorted().toString()
+
+            if (sortedPassword in words) {
+                continue@password
+            } else {
+                words.add(sortedPassword)
+            }
+        }
+
+        count++
+    }
+
+    println(count)
+}
---
+++ 2017/04/README.md
@@ -0,0 +1,6 @@
+# [Day 4 of 2017](https://adventofcode.com/2017/day/4)
+
+## Expected Results
+
+- Part 1: 383
+- Part 2: 265
