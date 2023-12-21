0f7e3d75001318c1a0f2065fbd8886c8662d62b4
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-21 22:56:34 +0100

    Add part 1 of day 21 of 2023

---
+++ 2023/21/21.kt
@@ -0,0 +1,60 @@
+import java.io.File
+import java.util.*
+
+private const val FILENAME = "2023/21/input.txt"
+
+val directions = listOf(
+    Pair(-1, 0),
+    Pair(1, 0),
+    Pair(0, -1),
+    Pair(0, 1),
+)
+
+fun main() {
+    partOne()
+}
+
+private fun partOne() {
+    val map = File(FILENAME).readLines()
+
+    var start = Pair(0, 0)
+
+    start@for ((row, line) in map.withIndex()) {
+        for ((column, character) in line.withIndex()) {
+            if (character == 'S') {
+                start = Pair(column, row)
+                break@start
+            }
+        }
+    }
+
+    val queue = LinkedList<Pair<Int, Int>>()
+    queue.push(start)
+
+    repeat(64) {
+        repeat(queue.size) {
+            val position = queue.remove()
+
+            for (direction in directions) {
+                val newX = position.first + direction.first
+                val newY = position.second + direction.second
+
+                if (newX < 0 || newY < 0) {
+                    continue
+                }
+
+                if (newX >= map[0].length || newY >= map.size) {
+                    continue
+                }
+
+                if (map[newY][newX] in listOf('.', 'S')) {
+                    if (!queue.contains(Pair(newX, newY))) {
+                        queue.add(Pair(newX, newY))
+                    }
+                }
+            }
+        }
+    }
+
+    println(queue.size)
+}
---
+++ 2023/21/README.md
@@ -0,0 +1,6 @@
+# [Day 21 of 2023](https://adventofcode.com/2023/day/21)
+
+## Expected Results
+
+- Part 1: 3658
+- Part 2: TODO
