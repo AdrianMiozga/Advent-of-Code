1afd1e17731f9f6f0e5c3037abf03cf0889f2250
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-21 01:56:04 +0100

    Add day 5 of 2017

---
+++ 2017/05/05.kt
@@ -0,0 +1,49 @@
+import java.io.File
+
+private const val FILENAME = "2017/05/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines().map { it.toInt() }.toMutableList()
+
+    var current = 0
+    var steps = 0
+
+    while (current < file.size) {
+        val jump = file[current]
+
+        file[current]++
+        current += jump
+
+        steps++
+    }
+
+    println(steps)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines().map { it.toInt() }.toMutableList()
+
+    var current = 0
+    var steps = 0
+
+    while (current < file.size) {
+        val jump = file[current]
+
+        if (jump >= 3) {
+            file[current]--
+        } else {
+            file[current]++
+        }
+
+        current += jump
+
+        steps++
+    }
+
+    println(steps)
+}
---
+++ 2017/05/README.md
@@ -0,0 +1,6 @@
+# [Day 5 of 2017](https://adventofcode.com/2017/day/5)
+
+## Expected Results
+
+- Part 1: 372139
+- Part 2: 29629538
