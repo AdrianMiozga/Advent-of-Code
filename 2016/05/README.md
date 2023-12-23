5983ac9ceb5808b9466b4e906b7e359f6cc02e5e
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-23 02:29:10 +0100

    Add day 5 of 2016

---
+++ 2016/05/README.md
@@ -0,0 +1,6 @@
+# [Day 5 of 2016](https://adventofcode.com/2016/day/5)
+
+## Expected Results
+
+- Part 1: 1a3099aa
+- Part 2: 694190cd
---
+++ 2016/05/Solution.kt
@@ -0,0 +1,68 @@
+import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
+
+private const val FILENAME = "2016/05/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val doorID = File(FILENAME).readText()
+
+    val messageDigest = MessageDigest.getInstance("MD5")
+    var password = ""
+
+    var i = 0
+
+    repeat(8) {
+        while (true) {
+            i++
+            val input = "$doorID$i"
+
+            val md5 = BigInteger(
+                1, messageDigest.digest(input.toByteArray())
+            ).toString(16).padStart(32, '0')
+
+            if (md5.startsWith("00000")) {
+                password += md5[5]
+                break
+            }
+        }
+    }
+
+    println(password)
+}
+
+private fun partTwo() {
+    val doorID = File(FILENAME).readText()
+
+    val messageDigest = MessageDigest.getInstance("MD5")
+    val password = CharArray(8) { '_' }
+
+    var i = 0
+
+    repeat(8) {
+        while (true) {
+            i++
+            val input = "$doorID$i"
+
+            val md5 = BigInteger(
+                1, messageDigest.digest(input.toByteArray())
+            ).toString(16).padStart(32, '0')
+
+            if (md5.startsWith("00000") && md5[5].isDigit()) {
+                val position = md5[5].digitToInt()
+
+                if (position <= 7 && password[position] == '_') {
+                    password[position] = md5[6]
+                    break
+                }
+            }
+        }
+    }
+
+    println(password)
+}
