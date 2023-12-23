85bb06297068369b310579f5a31f2227cae8f159
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-23 02:39:36 +0100

    Add day 6 of 2016

---
+++ 2016/06/README.md
@@ -0,0 +1,6 @@
+# [Day 6 of 2016](https://adventofcode.com/2016/day/6)
+
+## Expected Results
+
+- Part 1: cyxeoccr
+- Part 2: batwpask
---
+++ 2016/06/Solution.kt
@@ -0,0 +1,44 @@
+import java.io.File
+
+private const val FILENAME = "2016/06/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+
+    val transposed = transpose(file)
+
+    for (line in transposed) {
+        print(line.toList().groupingBy { it }.eachCount().maxBy { it.value }.key)
+    }
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines()
+
+    val transposed = transpose(file)
+
+    for (line in transposed) {
+        print(line.toList().groupingBy { it }.eachCount().minBy { it.value }.key)
+    }
+}
+
+private fun transpose(list: List<String>): MutableList<String> {
+    val result = mutableListOf<String>()
+
+    for (column in list[0].indices) {
+        var columnNew = ""
+
+        for (row in list.indices) {
+            columnNew += list[row][column]
+        }
+
+        result.add(columnNew)
+    }
+
+    return result
+}
