73112aeeb512e4775d1f1d689ea4e17df8b945b4
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-25 14:51:21 +0100

    Add day 8 of 2015

---
+++ 2015/08/README.md
@@ -0,0 +1,6 @@
+# [Day 8 of 2015](https://adventofcode.com/2015/day/8)
+
+## Expected Results
+
+- Part 1: 1333
+- Part 2: 2046
---
+++ 2015/08/Solution.kt
@@ -0,0 +1,67 @@
+import java.io.File
+
+private const val FILENAME = "2015/08/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+
+    var result = 0
+
+    for (line in file) {
+        var dataCharacters = 0
+
+        var index = 0
+
+        while (index < line.length) {
+            if (line[index] == '\\') {
+                dataCharacters++
+
+                val nextCharacter = line[index + 1]
+
+                when (nextCharacter) {
+                    '"' -> index += 2
+                    '\\' -> index += 2
+                    'x' -> index += 4
+                }
+            } else if (line[index] == '"') {
+                index++
+            } else {
+                index++
+                dataCharacters++
+            }
+        }
+
+        result += line.length
+        result -= dataCharacters
+    }
+
+    println(result)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines()
+
+    var result = 0
+
+    for (line in file) {
+        var encodedCharacters = 2
+
+        for (character in line) {
+            if (character in listOf('"', '\\')) {
+                encodedCharacters += 2
+            } else {
+                encodedCharacters++
+            }
+        }
+
+        result += encodedCharacters
+        result -= line.length
+    }
+
+    println(result)
+}
