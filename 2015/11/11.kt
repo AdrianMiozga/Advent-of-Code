aa41dced69d46dc04089b08583b2cad03bef1aeb
Adrian Miozga <AdrianMiozga@outlook.com>
2024-01-04 15:41:48 +0100

    Add day 11 of 2015

---
+++ 2015/11/11.kt
@@ -0,0 +1,90 @@
+import java.io.File
+
+private const val FILENAME = "2015/11/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    calculate(File(FILENAME).readText())
+}
+
+private fun partTwo() {
+    calculate("hxbxxyzz")
+}
+
+private fun incrementString(string: String): String {
+    var result = ""
+    var add = true
+
+    for (letter in string.reversed()) {
+        if (add) {
+            if (letter.code + 1 > 122) {
+                result += 'a'
+                add = true
+            } else {
+                result += (letter.code + 1).toChar()
+                add = false
+            }
+        } else {
+            result += letter
+        }
+    }
+
+    if (add) {
+        result += 'a'
+    }
+
+    return result.reversed()
+}
+
+private fun calculate(password: String) {
+    var next = password
+
+    while (true) {
+        next = incrementString(next)
+
+        var firstRule = true
+
+        if (next.contains(Regex("""[iol]"""))) {
+            firstRule = false
+        }
+
+        var secondRule = false
+
+        for (index in 0 until next.length - 2) {
+            if (next[index + 1].code == next[index].code + 1 && next[index + 2].code == next[index].code + 2) {
+                secondRule = true
+                break
+            }
+        }
+
+        var thirdRule = false
+        val set = HashSet<String>()
+
+        var index = 0
+
+        while (index < next.length - 1) {
+            if (next[index] == next[index + 1]) {
+                set.add("${next[index]}${next[index + 1]}")
+            }
+
+            if (index < next.length - 2 && next[index + 2] == next[index]) {
+                index += 2
+            } else {
+                index++
+            }
+        }
+
+        if (set.size > 1) {
+            thirdRule = true
+        }
+
+        if (firstRule && secondRule && thirdRule) {
+            println(next)
+            break
+        }
+    }
+}
---
+++ 2015/11/README.md
@@ -0,0 +1,6 @@
+# [Day 11 of 2015](https://adventofcode.com/2015/day/11)
+
+## Expected Results
+
+- Part 1: hxbxxyzz
+- Part 2: hxcaabcc
