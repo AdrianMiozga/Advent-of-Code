a3e1beff6ab1a7e42ca6f108b4b144aa04bbbcf1
Adrian Miozga <AdrianMiozga@outlook.com>
2024-12-02 10:07:40 +0100

    Add day 1 of 2024

---
+++ 2024/01/README.md
@@ -0,0 +1,6 @@
+# [Day 1 of 2024](https://adventofcode.com/2024/day/1)
+
+## Expected Results
+
+- Part 1: 1603498
+- Part 2: 25574739
---
+++ 2024/01/Solution.kt
@@ -0,0 +1,60 @@
+import java.io.File
+import kotlin.math.abs
+
+private const val FILENAME = "2024/01/input.txt"
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+
+    val leftArray = mutableListOf<Int>()
+    val rightArray = mutableListOf<Int>()
+
+    for (line in file) {
+        val (left, right) = line.split(Regex("\\s+")).map { it.toInt() }
+        leftArray.add(left)
+        rightArray.add(right)
+    }
+
+    leftArray.sort()
+    rightArray.sort()
+
+    var result = 0
+
+    for (i in 0 until leftArray.size) {
+        result += abs(leftArray[i] - rightArray[i])
+    }
+
+    println(result)
+}
+
+private fun partTwo() {
+    val file = File(FILENAME).readLines()
+
+    val leftArray = mutableListOf<Int>()
+    val occurrences = hashMapOf<Int, Int>()
+
+    for (line in file) {
+        val (left, right) = line.split(Regex("\\s+")).map { it.toInt() }
+
+        leftArray.add(left)
+
+        if (occurrences.containsKey(right)) {
+            occurrences[right] = (occurrences[right]!! + 1)
+        } else {
+            occurrences[right] = 1
+        }
+    }
+
+    var result = 0
+
+    for (element in leftArray) {
+        result += (element * occurrences.getOrDefault(element, 0))
+    }
+
+    print(result)
+}
