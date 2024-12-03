202299b052e9f7017d955c69dda8e32e816df703
Adrian Miozga <AdrianMiozga@outlook.com>
2024-12-03 22:17:04 +0100

    Add day 10 of 2022

---
+++ 2022/10/README.md
@@ -0,0 +1,6 @@
+# [Day 10 of 2022](https://adventofcode.com/2022/day/10)
+
+## Expected Results
+
+- Part 1: 14560
+- Part 2: TODO
---
+++ 2022/10/Solution.kt
@@ -0,0 +1,43 @@
+import java.io.File
+
+private const val FILENAME = "2022/10/input.txt"
+
+fun main() {
+    partOne()
+}
+
+private fun partOne() {
+    val file = File(FILENAME).readLines()
+
+    var x = 1
+    var cycle = 1
+    var line = 0
+    var secondTime = false
+    var result = 0
+
+    while (cycle < 220) {
+        cycle++
+
+        if (file[line] == "noop") {
+            line++
+        } else {
+            if (secondTime) {
+                x += file[line].split(" ").last().toInt()
+                line++
+                secondTime = false
+            } else {
+                secondTime = true
+            }
+        }
+
+        if (line >= file.size) {
+            line = 0
+        }
+
+        if ((cycle - 20) % 40 == 0) {
+            result += cycle * x
+        }
+    }
+
+    println(result)
+}
